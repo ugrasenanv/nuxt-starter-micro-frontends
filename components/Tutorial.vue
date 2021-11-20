2bbec15e272c19714c2b12c2ef67c8745250dae9
ugrasenanv <ugrasenanv@gmail.com>
2021-11-20 20:47:24 +0530

    1 file modified

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,20 +1,5 @@
-<!-- Please remove this file from your project -->
 <template>
-  <div
-    class="
-      relative
-      flex
-      items-top
-      justify-center
-      min-h-screen
-      bg-gray-100
-      sm:items-center sm:pt-0
-    "
-  >
-    testing........
-    <link
-      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
-      rel="stylesheet"
-    />
+  <div>
+    <h1>Hello from Vue</h1>
   </div>
 </template>
