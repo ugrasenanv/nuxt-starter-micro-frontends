4dedadc74cde3beb1bd66794e25f4b9df71edbd2
ugrasenanv <ugrasenanv@gmail.com>
2021-11-20 20:40:18 +0530

    2 files modified

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -12,9 +12,9 @@
     "
   >
     testing........
-    <!-- <link
+    <link
       href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
       rel="stylesheet"
-    /> -->
+    />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <!-- <Tutorial/> -->
+  <Tutorial />
 </template>
